1b92f111d2e2c3ed4ee98b62975c2a5ca5c0d5eb
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-19 21:20:01 +1100

    Started backend design
    Still not working at the moment but added some key files.

---
+++ GroupTherapyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroupTherapyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Family_Group()
+        {
+            return View();
+        }
+
+        public IActionResult Member_Profile()
+        {
+            return View();
+        }
+    }
+}
--- GroupTherapyWebApp/Program.cs
+++ GroupTherapyWebApp/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
